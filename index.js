15db28de09c3216e5ed15495f45dcbb98b29564d
ricod3 <rb.itadv@icloud.com>
2024-02-20 13:03:49 +0100

    modified index.js - refactored a few functions

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let request = indexedDB.open('myDatabse', 1);
 request.onupgradeneeded = function (event) {
     db = event.target.result;
     let languages = db.createObjectStore('languages', {autoIncrement: true});
-    let newObjectStore = db.createObjectStore('newObjectStore', {autoIncrement: true});
 }
 
 request.onsuccess = function (event) {
@@ -11,56 +10,69 @@ request.onsuccess = function (event) {
     displayData();
 }
 request.onerror = function (event) {
-    alert('error opening database' + event.target.errorCode);
+    alert("Datenbank konnte nicht geöffnet werden!" + event.target.errorCode);
+}
+
+// start a readwrite transaction and reference object store 'languages'
+function startReadWriteTransaction() {
+    let transaction = db.transaction(['languages'], 'readwrite');
+    let store = transaction.objectStore('languages');
+    return {transaction, store};
+}
+
+// start a readonly transaction and reference object store 'languages'
+function startReadOnlyTransaction() {
+    let transaction = db.transaction(['languages'], 'readonly');
+    let store = transaction.objectStore('languages');
+    return {transaction, store};
+}
+
+function iterateOverTable(allLanguages, table) {
+    for (let i = 0; i < allLanguages.length; i++) {
+        let row = table.insertRow();
+        row.insertCell().textContent = allLanguages[i].name;
+        row.insertCell().textContent = allLanguages[i].description;
+    }
 }
 
 // function to add new values
 function add() {
-    let x = db.transaction(['languages'], 'readwrite');
-    let store = x.objectStore('languages');
+    let {transaction, store} = startReadWriteTransaction();
     let language = {name: document.getElementById('name').value, description: document.getElementById('description').value};
     store.add(language);
-    x.oncomplete = function () {displayData();}
-    x.onerror = function(event) {
-        alert('error storing note ' + event.target.errorCode);
+    transaction.oncomplete = function () {displayData();}
+    transaction.onerror = function(event) {
+        alert("Datensatz konnte nicht hinzugefügt werden!" + event.target.errorCode);
     }
 }
 
 function removeAll() {
-    let x = db.transaction(['languages'], 'readwrite');
-    let store = x.objectStore('languages');
+    let {transaction, store} = startReadWriteTransaction();
     store.clear();
-    x.oncomplete = function () {displayData();}
-    x.onerror = function (event) {
-        alert('error storing note' + event.target.errorCode);
+    transaction.oncomplete = function () {displayData();}
+    transaction.onerror = function (event) {
+        alert("Datensätze konnten nicht gelöscht werden!" + event.target.errorCode);
+        displayData();
     }
 }
 
 function remove() {
-    let id = prompt("Bitte geben Sie die ID des Datensatzes ein, den Sie löschen möchten: ");
-    let x = db.transaction(['languages'], 'readwrite');
-    let store = x.objectStore('languages');
-    let request = store.get(Number(id));
+    let id = prompt("Bitte geben Sie die ID des Datensatzes ein, den Sie löschen möchten!");
+    let {transaction, store} = startReadWriteTransaction();
+    let request = store.delete(Number(id));
     request.onsuccess = function (event) {
-        let data = event.target.result;
-        if (data) {
-            let removeData = {id:Number(id)};
-            store.clear(removeData, Number(id));
-            x.oncomplete = function () {displayData();}
-        }
-        else {
-            alert("Kein Datensatz mit der angegebenen ID gefundenn!")
-        }
+        alert("Datensatz erfolgreich gelöscht!");
+        displayData();
     }
     request.onerror = function (event) {
-        alert('error getting data ' + event.target.errorCode);
+        alert("Fehler beim löschen des Datensatzes " + event.target.errorCode);
     }
 }
 
+
 function update() {
     let id = prompt("Bitte geben Sie die ID des Datensatzes ein, den Sie ändern möchten:");
-    let x = db.transaction(['languages'], 'readwrite');
-    let store = x.objectStore('languages');
+    let {transaction, store} = startReadWriteTransaction();
     let request = store.get(Number(id));
     request.onsuccess = function(event) {
         let data = event.target.result;
@@ -69,24 +81,19 @@ function update() {
             let newDescription = prompt("Bitte geben Sie die neue Beschreibunng ein:", data.description);
             let updatedData = {name: newName, description: newDescription, id: Number(id)};
             store.put(updatedData, Number(id));
-            x.oncomplete = function() { displayData(); }
+            transaction.oncomplete = function() { displayData(); }
         } else {
             alert("Kein Datensatz mit der angegebenen ID gefunden.");
         }
     }
-    request.onerror = function(event) {
-        alert('error getting data ' + event.target.errorCode);
-    }
 }
-
 
 // function for displaying data in the browser / UI
 function displayData() {
-    let x = db.transaction(['languages'],  'readonly');
-    let store = x.objectStore('languages');
-    let req = store.openCursor();
+    let {transaction, store} = startReadOnlyTransaction();
+    let request = store.openCursor();
     let allLanguages = [];
-    req.onsuccess = function (event) {
+    request.onsuccess = function (event) {
         let cursor = event.target.result;
         if (cursor != null) {
             allLanguages.push(cursor.value);
@@ -95,11 +102,7 @@ function displayData() {
             let table = document.getElementById('table');
             table.innerHTML = "<tr><th>Name</th><th>Beschreibung</th></tr>"
 
-            for (let i = 0; i < allLanguages.length; i++) {
-                let row = table.insertRow();
-                row.insertCell().textContent = allLanguages[i].name;
-                row.insertCell().textContent = allLanguages[i].description;
-            }
+            iterateOverTable(allLanguages, table);
         }
     }
 }
